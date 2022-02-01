fd8b539a0d919f43d33981cd15557ade2887c837
Said Azizov <said_azizov2004@mail.ru>
2022-02-01 21:42:50 +0300

    stop-lines detection task comleted

--- car_control/line_estimator/HorizontalLines.py
+++ car_control/line_estimator/HorizontalLines.py
@@ -1,90 +1,70 @@
-from matplotlib import image
-import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-from PerspectiveTransformation import PerspectiveTransformation
-from LaneLines import LaneLines
-from Thresholding import Thresholding
-from PerspectiveTransformation import PerspectiveTransformation
 
 class HorizontalLines:
-    def __init__(self, ):
+    def __init__(self, image_size, kn = 15, threshold = 20):
         '''
         This class trying to detect 
         horizontal stop-line on the road
+
+        Arguments:
+
+        nk : int : number of rows to compute mean value 
+        threshold : int : threshold for sum of nk rows 
+        img_size : tuple : (img_height, img_width)
         '''
 
         # region of interest : list : [ty, tx, by, bx]
-        pass
+        self.kn = kn
+        self.threshold = threshold
+        self.image_size = image_size
         
-    def get_hist(self, image, kn = 15, threshold = 23):
+    def get_hist(self, image):
         '''
         input : binary image
         output : histogram of white pixels distribution
         '''
         plot = cv2.merge([np.ones_like(image)] * 3)
         sums = np.sum(image / 255, axis = 1).astype(np.float32)
-        sums = [int(sum(sums[index:index + kn]) / kn) for index in range(len(sums - kn))]
+        sums = [int(sum(sums[index:index + self.kn]) / self.kn) for index in range(len(sums - self.kn))]
         for index, value in enumerate(sums):
-            if value > threshold:
+            if value > self.threshold:
                 plot[index, :] *= (0, 0, 255)
             else:
                 plot[index, :value] *= (255 - min(value*10, 255), 255 - min(value*10, 255), min(value*10, 255))
         return plot
 
-    def mark_line(self, image, kn = 10, threshold = 23):
+    def mark_line(self, image):
         '''
-        input : binary image
-        output : 3D image with marked road
+        Arguments : 
+        
+        image : np.ndarray : array of size (H, W)
+        
+        Outputs: image of size  (H, W) with marked road
         '''
+        mask = np.zeros_like(image)
         sums = np.sum(image / 255, axis = 1).astype(np.float32)
-        sums = [int(sum(sums[index:index + kn]) / kn) for index in range(len(sums - kn))]
-        image = cv2.merge([image]*3).astype(np.float32)
+        sums = [int(sum(sums[index:index + self.kn]) / self.kn) for index in range(len(sums - self.kn))]
+        
+        max_index = 0
         for index, value in enumerate(sums):
-            if value > threshold:
-                image[index, :] *= (0, 0, 255)
-            else:
-                image[index, :value] *= (255 - min(value*10, 255), 255 - min(value*10, 255), min(value*10, 255))
-        return image
+            if value > self.threshold:
+                mask[index, :] = 255
+                if max_index < index:
+                    max_index = index
+
+        distance = self.image_size[0] - max_index if max_index else None
+        print(f'distance: {distance}')
+        return mask, distance
     
     def forward(self, image):
         '''
-        inputs: 
-
+        Arguments: 
         image : np.ndarray
 
-        output : bool : is there stop-line ahead
+        output : tuple : (masked_image, distance)
         '''
 
         return self.mark_line(image)
-
-
-if __name__ == "__main__":
-
-    cap = cv2.VideoCapture('test_video.mov')
-
-    img_size = (280, 320)
-    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) 
-
-    HorizontalLinesDetector = HorizontalLines()
-    PerspectiveTransformationHelper = PerspectiveTransformation(img_size[::-1])
-    ThresholdingHelper = Thresholding()
-
-    for _ in range(num_frames):
-        success, frame = cap.read()
-        if success:
-            frame = cv2.resize(frame, img_size)
-            frame = PerspectiveTransformationHelper.forward(frame)
-            frame = ThresholdingHelper.forward(frame)
-            cv2.imshow('frame', frame)
-            frame = HorizontalLinesDetector.forward(frame)
-            cv2.imshow('hist', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            break
-
-        
-
         
